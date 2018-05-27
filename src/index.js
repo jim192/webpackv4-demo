fc8dd9e790bd11d78e12c5f23f1ac3300a850a91
Humingyang <humingyangjim@126.com>
2018-05-27 23:24:20 +0800

    updating the binding to the new function

--- src/index.js
+++ src/index.js
@@ -15,11 +15,18 @@ function component(){
 	return element;
 }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
+// Click the button on the page, the console is printing the old printMe function
+// To update that binding to the new printMe
+let element = component();// Store the element to re-render on print.js changes
+document.body.appendChild(element);
 
 if(module.hot){
 	module.hot.accept('./print.js',function(){
 		console.log('Accepting the updated printMe module!');
-		printMe();
+		// printMe();
+		document.body.removeChild(element);
+		element = component(); // Re-render the "component" to update the click handler
+		document.body.appendChild(element);
 	})
 }

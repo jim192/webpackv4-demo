5a0209b901334605f867c3d9043cb7b35223b4ed
Humingyang <humingyangjim@126.com>
2018-05-27 23:05:50 +0800

    enabling HMR

--- src/print.js
+++ src/print.js
@@ -1,5 +1,3 @@
 export default function printMe(){
-	console.log('I get called from print.js!');
-	//Make a mistake to test devtool:'eval-source-map'
-	// cosnole.log('I get called from print.js!');
+	console.log('Updating print.js!');
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
 	entry:{
-		app: './src/index.js',
-		print:'./src/print.js'
+		app: './src/index.js'
 	},
 	output:{
 		filename:'[name].bundle.js',
@@ -16,14 +16,16 @@ module.exports = {
 		new CleanWebpackPlugin(['dist']),
 		new HtmlWebpackPlugin({
 			title:'output Management with html-webpack-plugin'
-		})
+		}),
+		new webpack.NamedModulesPlugin(),// It easier to see which dependencies are being patched
+		new webpack.HotModuleReplacementPlugin()
 	],
 	// https://webpack.js.org/configuration/devtool/
-	devtool:'eval-source-map',
-	/*
+	devtool:'eval-source-map',	
 	//use webpack dev server
 	devServer:{
-		contentBase:'./dist'//Serve the file from the /dist/ on localhost:8080
+		contentBase:'./dist',//Serve the file from the /dist/ on localhost:8080
+		hot:true
 	}
-	*/
+	
 };

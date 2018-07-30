746914be1d724d697e5d1385f05a09820929e061
Humingyang <humingyangjim@126.com>
2018-07-30 16:32:03 +0800

    get filename with fs

--- config/htmlName.js
+++ config/htmlName.js
@@ -1,8 +1,24 @@
+//获取所有页面，生成页面集合
+//引入文件模块
+let fs = require("fs");
+const getFileNameList = path=> {
+	let fileList = [];
+	//返回指定目录下所有文件名称的数组对象
+	let dirList = fs.readdirSync(path);
+	dirList.forEach(item=>{
+		if (item.indexOf('html')>-1) {
+			fileList.push(item.split('.')[0]);
+		}
+	});
+	return fileList;
+}
+
 module.exports = {
-	HTMLDirs:[
-		"index",
-		"pageOne",
-		"pageTwo",
-		"pageThree"
-	],
+	// HTMLDirs:[
+	// 	"index",
+	// 	"pageOne",
+	// 	"pageTwo",
+	// 	"pageThree"
+	// ],
+	HTMLDirs:getFileNameList('./src/html')
 }
